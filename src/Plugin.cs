e242ad689f6797fdaceddf7e3bc171b994810e69
blbrdv <95272917+blbrdv@users.noreply.github.com>
2023-06-08 13:19:25 +0700

    init plugin

---
+++ src/Plugin.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+
+namespace Aegir
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
