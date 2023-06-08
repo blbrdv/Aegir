e0dcbdd638d46db78a11c42e91600bfe6fc2491b
blbrdv <95272917+blbrdv@users.noreply.github.com>
2023-06-08 19:18:57 +0700

    add patch

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,12 +1,22 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 
 namespace Aegir
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+        internal static new ManualLogSource Logger;
+
         private void Awake()
         {
+            Logger = base.Logger;
+
+            harmony.PatchAll();
+
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
